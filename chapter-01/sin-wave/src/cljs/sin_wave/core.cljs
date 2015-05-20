e69165561160d7e322f8aeac4f399538d8f81797
Raf Szalanski <rafal.szalanski@alliants.com>
2015-05-20 13:44:17 +0100

    Chapter 1 - it draws sinusoidal wave. Yay git add -p

--- chapter-01/sin-wave/src/cljs/sin_wave/core.cljs
+++ chapter-01/sin-wave/src/cljs/sin_wave/core.cljs
@@ -14,9 +14,22 @@
 ; starts at 0 and will grow every 10 ms
 (def time (interval 10))
 
-(.subscribe (.take time 5) (fn [n] (.log js/console n)))
+(defn deg-to-rad [n]
+  (* (/ Math/PI 180) n))
 
-(-> time
-  (.take 5)
-  (.subscribe (fn [n]
-                (.log js/console n))))
+(defn sine-coord [x]
+  (let [sin (Math/sin (deg-to-rad x))
+        y (- 100 (* sin 90))]
+    {:x x :y y :sin sin}))
+
+(def sine-wave
+  (.map time sine-coord))
+
+(defn fill-rect [x y colour]
+  (set! (.-fillStyle ctx) colour)
+  (.fillRect ctx x y 2 2))
+
+(-> sine-wave
+  (.take 600)
+  (.subscribe (fn [{:keys [x y]}]
+                (fill-rect x y "red"))))
