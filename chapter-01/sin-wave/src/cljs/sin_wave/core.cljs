cb0fd054958c434fb324b5cc188c5484caae51ff
Raf Szalanski <rafal.szalanski@alliants.com>
2015-05-20 13:20:41 +0100

    Chapter 1 - clean canvas

--- chapter-01/sin-wave/src/cljs/sin_wave/core.cljs
+++ chapter-01/sin-wave/src/cljs/sin_wave/core.cljs
@@ -1,3 +1,9 @@
 (ns sin-wave.core)
 
 (defn main [])
+
+(def canvas (.getElementById js/document "myCanvas"))
+
+(def ctx (.getContext canvas "2d"))
+
+(.clearReact ctx 0 0 (.-width canvas) (.-height canvas))
