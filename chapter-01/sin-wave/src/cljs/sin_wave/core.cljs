0db32c72385f19fec8a60dc6e22bad53e07a1110
Raf Szalanski <rafal.szalanski@alliants.com>
2015-05-20 13:26:12 +0100

    Chapter 1 - warmup. vanilla clojure vs thread-first macro

--- chapter-01/sin-wave/src/cljs/sin_wave/core.cljs
+++ chapter-01/sin-wave/src/cljs/sin_wave/core.cljs
@@ -6,4 +6,17 @@
 
 (def ctx (.getContext canvas "2d"))
 
-(.clearReact ctx 0 0 (.-width canvas) (.-height canvas))
+(.clearRect ctx 0 0 (.-width canvas) (.-height canvas))
+
+(def interval js/Rx.Observable.interval)
+
+; Infinite sequence of numbers
+; starts at 0 and will grow every 10 ms
+(def time (interval 10))
+
+(.subscribe (.take time 5) (fn [n] (.log js/console n)))
+
+(-> time
+  (.take 5)
+  (.subscribe (fn [n]
+                (.log js/console n))))
